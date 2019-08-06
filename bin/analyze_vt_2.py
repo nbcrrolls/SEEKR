6c8a19072d10cd1e755056d4124dfb5c28917a35
Benjamin R Jagger <benjaminrjagger@Benjamins-MacBook-Pro-3.local>
2019-08-06 12:09:23 -0700

    more debugging prints

--- bin/analyze_vt_2.py
+++ bin/analyze_vt_2.py
@@ -782,7 +782,7 @@ def monte_carlo_milestoning_error(Q0, N_pre, R_pre, p_equil, T_tot, num = 1000, 
     while (Qnew[i][j] == 0.0 or i == j):
       i = random.randint(0,m-1)
       j = random.randint(0,m-1)
-    #print "r" , r2, "i", i, "j", j
+    print "r" , r2, "i", i, "j", j
 
 
     #if Qnew[i][j] == 0.0: 
@@ -792,21 +792,19 @@ def monte_carlo_milestoning_error(Q0, N_pre, R_pre, p_equil, T_tot, num = 1000, 
       #print "skip"
     #  continue
       
-    #print "q_ij", Qnew[i,j]
+    print "q_ij", Qnew[i,j]
     Q_gamma = gamma.rvs(N[i,j], scale = 1/R[i])
-    #print "gamma", Q_gamma
+    print "gamma", Q_gamma
 
     delta =  Qnew[i,j] - Q_gamma
-    #print "delta", delta
-
-    log_p_Q_new = N[i,j] * log(Qnew[i,j] - delta) + -(Qnew[i,j] - delta) * R[i] + -(Qnew[i,i] + delta) * R[i]
-
+    print "delta", delta
 
     log_p_Q_old = N[i,j] * log(Qnew[i,j]) + -Qnew[i,j] * R[i] + -Qnew[i,i] * R[i]
 
-      
-    #print "log P(Q_new)", log_p_Q_new
-    #print "log P(Q_old)", log_p_Q_old
+    log_p_Q_new = N[i,j] * log(Qnew[i,j] - delta) + -(Qnew[i,j] - delta) * R[i] + -(Qnew[i,i] + delta) * R[i]
+
+    print "log P(Q_new)", log_p_Q_new
+    print "log P(Q_old)", log_p_Q_old
 
       
     p_acc =  log_p_Q_new - log_p_Q_old
